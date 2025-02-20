7c316da16fa96bee2c85de2fb8d3b43c25cbfea2
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-20 17:41:07 -0600

    Modify AE script to download JSON from s3 bucket

--- automate_aftereffects.jsx
+++ automate_aftereffects.jsx
@@ -1,4 +1,63 @@
-var jsonFile = new File("most_recent_post.json");
+/**
+ * Downloads a file from a URL using a basic HTTP GET via a Socket.
+ * This function assumes the file is publicly accessible.
+ *
+ * @param {string} url - The URL of the file to download.
+ * @param {string} localPath - The local path where the file will be saved.
+ * @returns {boolean} True if download succeeds, false otherwise.
+ */
+function downloadFromUrl(url, localPath) {
+    try {
+        var socket = new Socket();
+        var urlParts = url.split("://");
+        if (urlParts.length < 2) {
+            throw new Error("Invalid URL format.");
+        }
+        var hostAndPath = urlParts[1].split("/");
+        var host = hostAndPath.shift();
+        var path = "/" + hostAndPath.join("/");
+        if (socket.open(host + ":80", "BINARY")) {
+            var request = "GET " + path + " HTTP/1.0\r\nHost: " + host + "\r\n\r\n";
+            socket.write(request);
+            var response = socket.read(999999);
+            socket.close();
+            // Split HTTP headers from content
+            var parts = response.split("\r\n\r\n");
+            if (parts.length < 2) {
+                throw new Error("Invalid HTTP response.");
+            }
+            var content = parts.slice(1).join("\r\n\r\n");
+            var file = new File(localPath);
+            file.encoding = "UTF-8";
+            if (!file.open("w")) {
+                throw new Error("Failed to open local file for writing.");
+            }
+            file.write(content);
+            file.close();
+            return true;
+        } else {
+            throw new Error("Unable to open socket connection.");
+        }
+    } catch(e) {
+        alert("Download failed: " + e.message);
+        return false;
+    }
+}
+
+// Define the S3 URL (update with your bucket name and region as needed)
+var s3JsonUrl = "https://your-s3-bucket.s3.amazonaws.com/most_recent_post.json";
+var localJsonPath = "most_recent_post.json";
+
+// Download the JSON file from S3 if it does not exist locally
+var jsonFile = new File(localJsonPath);
+if (!jsonFile.exists) {
+    var success = downloadFromUrl(s3JsonUrl, localJsonPath);
+    if (!success) {
+        alert("Failed to download JSON from S3.");
+        // Exit script if JSON cannot be obtained
+        return;
+    }
+}
 
 if (jsonFile.exists) {
     jsonFile.open("r");
@@ -16,7 +75,7 @@ if (jsonFile.exists) {
             var compName = "standard-news-template";
             for (var i = 1; i <= app.project.items.length; i++) {
                 var item = app.project.items[i];
-                if (item instanceof CompItem && item.name == compName) {
+                if (item instanceof CompItem && item.name === compName) {
                     comp = item;
                     break;
                 }
@@ -32,13 +91,13 @@ if (jsonFile.exists) {
 
             for (var i = 1; i <= comp.layers.length; i++) {
                 var layer = comp.layers[i];
-                if (layer.name == "Title") {
+                if (layer.name === "Title") {
                     titleLayer = layer;
                 }
-                if (layer.name == "Description") {
+                if (layer.name === "Description") {
                     subtitleLayer = layer;
                 }
-                if (layer.name == "BackgroundImage") {
+                if (layer.name === "BackgroundImage") {
                     backgroundLayer = layer;
                 }
             }
@@ -76,7 +135,7 @@ if (jsonFile.exists) {
 
                 backgroundLayer.transform.position.setValue([compWidth / 2, compHeight / 2]);
             } else {
-                throw new Error("Background image (backgroundimage.jpg) not found.");
+                throw new Error("Background image (backgroundimage_converted.jpg) not found.");
             }
 
             var renderQueue = app.project.renderQueue;
@@ -87,9 +146,7 @@ if (jsonFile.exists) {
             outputModule.file = new File("anime_post.mp4");
 
             outputModule.format = "QuickTime";
-
             outputModule.videoCodec = "H.264";
-
             outputModule.includeSourceXMP = false; 
             outputModule.audioOutput = true; 
             outputModule.audioCodec = "AAC"; 
