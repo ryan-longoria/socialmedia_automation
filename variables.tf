3dfbdcd3978c4c9c418589b0d25cf3c6c4771765
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-20 21:35:33 -0600

    Add variables terraform file

---
+++ variables.tf
@@ -0,0 +1,38 @@
+################################################################################
+## Terraform Variables
+################################################################################
+
+variable "aws_account_id" {
+  description = "The AWS Account ID. Used for configuring the provider and defining ECS resources."
+  type = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources."
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket to store media assets."
+  type        = string
+  default     = "my-anime-media-bucket-unique"
+}
+
+variable "environment" {
+  description = "The environment name (e.g., dev, prod)."
+  type        = string
+  default     = "dev"
+}
+
+variable "ec2_instance_id" {
+  description = "The ID of the Windows EC2 instance with Photoshop installed."
+  type        = string
+  default     = "i-your-ec2-instance-id"
+}
+
+variable "terraform_backend_bucket" {
+  description = "The S3 bucket used to store Terraform state."
+  type        = string
+  default     = "my-terraform-backend-bucket-unique"
+}
