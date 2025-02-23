45d6f864e180b63cd5c0d6df1ae9069fac834255
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-23 13:54:39 -0600

    Remove assume_role block

--- providers.tf
+++ providers.tf
@@ -5,10 +5,6 @@
 provider "aws" {
   region = var.aws_region
 
-  assume_role {
-    role_arn = "arn:aws:iam::851725522400:role/Atlantis-EC2-Role"
-  }
-
   default_tags {
     tags = {
       DeployedBy = "Terraform"
