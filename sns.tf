c15db7d426fa0fa53c1ea07ee6176d7cab6eeb4d
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-24 15:08:16 -0600

    Modify topic_arn and resource name

--- sns.tf
+++ sns.tf
@@ -2,8 +2,8 @@ resource "aws_sns_topic" "anime_notifications" {
   name = "anime_post_notifications"
 }
 
-resource "aws_sns_topic_subscription" "animeutopia_subscription" {
-  topic_arn = aws_sns_topic.animeutopia_notifications.arn
+resource "aws_sns_topic_subscription" "anime_subscription" {
+  topic_arn = aws_sns_topic.anime_notifications.arn
   protocol  = "email"
   endpoint  = "kk47mgmt@outlook.com"
 }
