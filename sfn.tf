22c9ecab948f14d3e6a122ac3b290232bc4119c0
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-24 14:06:11 -0600

    Add new lambda arn's for starting/stopping ec2 to sfn workflow

--- sfn.tf
+++ sfn.tf
@@ -61,7 +61,9 @@ resource "aws_sfn_state_machine" "anime_workflow" {
     process_content_arn = aws_lambda_function.process_content.arn,
     store_data_arn      = aws_lambda_function.store_data.arn,
     render_video_arn    = aws_lambda_function.render_video.arn,
-    save_video_arn      = aws_lambda_function.save_video.arn
+    save_video_arn      = aws_lambda_function.save_video.arn,
+    start_instance_arn  = aws_lambda_function.start_instance.arn,
+    stop_instance_arn   = aws_lambda_function.stop_instance.arn
   })
 }
 
