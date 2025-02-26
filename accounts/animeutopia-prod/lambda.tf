016c8b39072b03096c0ec103710eb5b1ca0192bc
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-26 15:34:26 -0600

    Added source_code_hash attribute to each lambda function

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -52,7 +52,7 @@ resource "aws_iam_policy" "ssm_send_command_policy" {
       {
         Effect : "Allow",
         Action : "ssm:SendCommand",
-        Resource = "*" // You could scope this further to your EC2 instance if needed.
+        Resource : "*"  // You could scope this further to your EC2 instance if needed.
       }
     ]
   })
@@ -143,6 +143,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_lambda_function" "fetch_rss" {
   function_name = "fetch_rss"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -152,6 +153,7 @@ resource "aws_lambda_function" "fetch_rss" {
 resource "aws_lambda_function" "process_content" {
   function_name = "process_content"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -171,6 +173,7 @@ resource "aws_lambda_function" "process_content" {
 resource "aws_lambda_function" "store_data" {
   function_name = "store_data"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -186,6 +189,7 @@ resource "aws_lambda_function" "store_data" {
 resource "aws_lambda_function" "render_video" {
   function_name = "render_video"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -201,6 +205,7 @@ resource "aws_lambda_function" "render_video" {
 resource "aws_lambda_function" "save_video" {
   function_name = "save_video"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -217,6 +222,7 @@ resource "aws_lambda_function" "save_video" {
 resource "aws_lambda_function" "start_instance" {
   function_name = "start_instance"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -232,6 +238,7 @@ resource "aws_lambda_function" "start_instance" {
 resource "aws_lambda_function" "stop_instance" {
   function_name = "stop_instance"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
@@ -247,6 +254,7 @@ resource "aws_lambda_function" "stop_instance" {
 resource "aws_lambda_function" "notify_post" {
   function_name = "notify_post"
   filename      = "${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip"
+  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip")
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
   role          = aws_iam_role.lambda_role.arn
