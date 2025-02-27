c67793c0a0ae765abc69e9ecb081d3a755966651
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-02-27 11:24:24 -0600

    Update iam policy to allow access to buckets

--- accounts/animeutopia-prod/lambda.tf
+++ accounts/animeutopia-prod/lambda.tf
@@ -89,20 +89,18 @@ resource "aws_iam_role_policy_attachment" "attach_ec2_describe_policy" {
 }
 
 resource "aws_iam_policy" "s3_list_and_get_policy" {
-  name        = "anime_s3_list_and_get_policy"
-  description = "Policy to allow Lambda to list the bucket and get objects"
+  name        = "anime_s3_full_policy"
+  description = "Policy to allow Lambda full access to S3 for the prod-animeutopia-media-bucket"
   policy      = jsonencode({
     Version: "2012-10-17",
     Statement: [
       {
         Effect: "Allow",
-        Action: ["s3:ListBucket"],
-        Resource: "arn:aws:s3:::prod-animeutopia-media-bucket"
-      },
-      {
-        Effect: "Allow",
-        Action: ["s3:GetObject"],
-        Resource: "arn:aws:s3:::prod-animeutopia-media-bucket/*"
+        Action: "s3:*",
+        Resource: [
+          "arn:aws:s3:::prod-animeutopia-media-bucket",
+          "arn:aws:s3:::prod-animeutopia-media-bucket/*"
+        ]
       }
     ]
   })
@@ -141,23 +139,23 @@ data "aws_caller_identity" "current" {}
 # Lambda Functions
 #############################
 resource "aws_lambda_function" "fetch_rss" {
-  function_name = "fetch_rss"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "fetch_rss"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/fetch_rss/fetch_rss.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 }
 
 resource "aws_lambda_function" "process_content" {
-  function_name = "process_content"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "process_content"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/process_content/process_content.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 
   environment {
     variables = {
@@ -171,13 +169,13 @@ resource "aws_lambda_function" "process_content" {
 }
 
 resource "aws_lambda_function" "store_data" {
-  function_name = "store_data"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "store_data"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/store_data/store_data.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 
   environment {
     variables = {
@@ -187,13 +185,13 @@ resource "aws_lambda_function" "store_data" {
 }
 
 resource "aws_lambda_function" "render_video" {
-  function_name = "render_video"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 20
+  function_name      = "render_video"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/render_video/render_video.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 20
 
   environment {
     variables = {
@@ -203,13 +201,13 @@ resource "aws_lambda_function" "render_video" {
 }
 
 resource "aws_lambda_function" "save_video" {
-  function_name = "save_video"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 20
+  function_name      = "save_video"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/save_video/save_video.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 20
 
   environment {
     variables = {
@@ -220,13 +218,13 @@ resource "aws_lambda_function" "save_video" {
 }
 
 resource "aws_lambda_function" "start_instance" {
-  function_name = "start_instance"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "start_instance"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/start_instance/start_instance.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 
   environment {
     variables = {
@@ -236,13 +234,13 @@ resource "aws_lambda_function" "start_instance" {
 }
 
 resource "aws_lambda_function" "stop_instance" {
-  function_name = "stop_instance"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "stop_instance"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/stop_instance/stop_instance.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 
   environment {
     variables = {
@@ -252,13 +250,13 @@ resource "aws_lambda_function" "stop_instance" {
 }
 
 resource "aws_lambda_function" "notify_post" {
-  function_name = "notify_post"
-  filename      = "${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip"
-  source_code_hash = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip")
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = aws_iam_role.lambda_role.arn
-  timeout       = 10
+  function_name      = "notify_post"
+  filename           = "${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip"
+  source_code_hash   = filebase64sha256("${path.module}/artifacts/scripts/AnimeUtopia/notify_post/notify_post.zip")
+  handler            = "lambda_function.lambda_handler"
+  runtime            = "python3.9"
+  role               = aws_iam_role.lambda_role.arn
+  timeout            = 10
 
   environment {
     variables = {
