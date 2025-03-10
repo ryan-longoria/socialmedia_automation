a21a0f9e78ed5533bf1240d60a81d5760e9c8827
ryan-longoria <ryan.longoria.cy@gmail.com>
2025-03-10 14:55:07 -0500

    Remove SNS terraform

--- accounts/animeutopia-prod/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "anime_notifications" {
-  name = "anime_post_notifications"
-}
-
-resource "aws_sns_topic_subscription" "anime_subscription" {
-  topic_arn = aws_sns_topic.anime_notifications.arn
-  protocol  = "email"
-  endpoint  = "kk47mgmt@outlook.com"
-}
